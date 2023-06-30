951cd920259f904da4622ab904821f277ba0c492
d3v-anaxa <d3v.anaxa@gmail.com>
2023-07-01 02:50:30 +0530

    code changed
    simplified the demonstration by just taking one user input

--- assignment/OverloadedFunctionsWithTypeConversion.java
+++ assignment/OverloadedFunctionsWithTypeConversion.java
@@ -1,24 +1,19 @@
 import java.util.Scanner;
 class OverloadedFunctionsWithTypeConversion{
-    public static void func(int x){
-        System.out.println( x + " --(int to double)--> " + (float)(x));
+    public static char func(short x){
+        System.out.println( x + " --(short to char)--> " + (char)(x));
+        return (char)(x);
     }
-    public static void func(double x){
-        System.out.println( x + " --(double to int)--> " + (int)(x));
+    public static void func(int x){
+        System.out.println( x + " --(int to double)--> " + (double)(x));
     }
     public static void main(String[] args){
         Scanner in = new Scanner(System.in);
-        float num_f;
         byte num_b;
-
         System.out.print("Enter a byte code -> ");
         num_b = in.nextByte();
-        func(num_b);
-
-        System.out.print("Enter a floating-point integer -> ");
-        num_f = in.nextFloat();
-        func(num_f);
-
         in.close();
+        char ch = func(num_b);
+        func(ch);
     }
 }
